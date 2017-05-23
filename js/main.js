411b2edd724933d17824618a2d2b3c68b6ba9700
maryGarL <maryel_565@hotmail.com>
2017-05-23 17:27:54 -0500

    a nada

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var cerrar = document.getElementById('closeAlert');
+cerrar.addEventListener("click", function () {
+  var alertTopClose = document.getElementById("alertTopClose");
+  alertTopClose.classList.add("closeAlertTop");
+
+});
